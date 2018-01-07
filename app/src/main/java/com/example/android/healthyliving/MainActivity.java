506ca5aece0f10e4165ef74c97ac4231020d0e7b
Chrisse Tiana <chrissetiana@gmail.com>
2018-01-07 06:29:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/healthyliving/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.android.healthyliving;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int healthLevel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void yes(View view) {
+        healthLevel += 1;
+        String message = "You answered yes,current health level is " + healthLevel;
+        display(message);
+    }
+
+    public void no(View view) {
+        healthLevel -= 1;
+        String message = "You answered no,current health level is " + healthLevel;
+        display(message);
+    }
+
+    public void sometimes(View view) {
+        healthLevel += 0;
+        String message = "You answered sometimes,current health level is " + healthLevel;
+        display(message);
+    }
+
+    public void display(String s){
+        TextView t = (TextView)findViewById(R.id.display_text_view);
+        t.setText(s);
+    }
+}
